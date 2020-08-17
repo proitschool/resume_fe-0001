672d799682cba7236035c551c4e240db264f0379
dimasemehuk7 <51905745+dimasemehuk7@users.noreply.github.com>
2020-08-17 19:01:23 +0300

    res-8 webpack config

---
+++ webapp/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
+};
