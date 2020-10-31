0221bd2439841d039634ae88350c98b524d89f60
dimasemehuk7 <51905745+dimasemehuk7@users.noreply.github.com>
2020-10-31 14:53:27 +0200

    res-32 add locals styles

--- webapp/src/store/ru.js
+++ webapp/src/store/ru.js
@@ -1,14 +1,17 @@
 module.exports = {
-    location: 'Location',
-    phone: 'Phone',
+    locals: [
+        {name: 'En', local: 'en', active: false},
+        {name: 'Ru', local: 'ru', active: true}
+    ],
+    location: 'Локализация',
+    phone: 'Телефон',
     web: 'Web',
     email: 'Email',
     intro: {
-        title: 'INTRO',
-        description: 'What I am all about',
+        title: 'Вводная часть',
+        description: 'Обо мне',
         content: `
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aaliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Nulla vitae mauris non felis mollis faucibus.
-            
             Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna. Fusce lacinia arcu et nulla. Nulla vitae mauris non felis mollis.
         `
     }
