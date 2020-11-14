fe53c97d86b16ff9300a6e01c901d210e3f23b52
dimasemehuk7 <51905745+dimasemehuk7@users.noreply.github.com>
2020-11-14 17:05:18 +0200

    RES-55 Config deaploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app.js');
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, '0.0.0.0', () => {
 	console.log(`App started on port: ${port}`);
 })
+
