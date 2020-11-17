28037d90cdf4ae3c4b768cbc60e4eb3f6a8e0572
mykhailo.zheliaskov <mzhelyaskov@gmail.com>
2020-11-17 21:35:44 +0100

    RES-60

--- src/app.js
+++ src/app.js
@@ -4,14 +4,16 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 const STATIC_ROOT_PATH = path.join(__dirname, '..', 'static');
+const DEFAULT_LOCAL = 'en';
+const AVAILABLE_LOCALS = new Set(['en', 'ru']);
 
 app.use('/static', express.static(STATIC_ROOT_PATH));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
 	let local = req.cookies.local;
-	if (!local) {
-		local = 'en';
+	if (!local || !AVAILABLE_LOCALS.has(local)) {
+		local = DEFAULT_LOCAL;
 		res.cookie('local', local);
 	}
 	res.sendFile(`${local}.html`, {root: STATIC_ROOT_PATH});
